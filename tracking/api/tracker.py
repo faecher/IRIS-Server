f504cb41900b302df78928ba2ada2880380084e7
Alexander Kaschta <alexander.kaschta9@kit.edu>
2025-07-04 14:11:13 +0200

    ADD: Tracker update endpoint

--- tracking/api/tracker.py
+++ tracking/api/tracker.py
@@ -1,9 +1,9 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from tracking.db.crud import get_trackers
+from tracking.db.crud import get_trackers, get_tracker_by_id
 from tracking.dependencies import get_db
 from tracking.models import TrackerModel
 
@@ -14,3 +14,15 @@ tracker_router = APIRouter(
 @tracker_router.get("/", response_model=List[TrackerModel])
 def get_tracker(db: Session = Depends(get_db)):
     return get_trackers(db)
+
+
+@tracker_router.post("/{instance_id}")
+def update_tracker(instance_id: int, db: Session = Depends(get_db)):
+    tracker = get_tracker_by_id(db, instance_id)
+
+    if tracker is None:
+        raise HTTPException(status_code=404, detail="Couldn't create instance")
+
+    # TODO: Update tracker
+
+    pass
