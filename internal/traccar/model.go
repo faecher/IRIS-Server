4d687f6ea61734b938f0cdbfe34e60387242afe4
XilefTech <github@xilef.tech>
2026-07-05 21:47:16 +0200

    Optimize mem layout

--- internal/traccar/model.go
+++ internal/traccar/model.go
@@ -10,12 +10,12 @@ type traccarMessage struct {
 
 // commented out fields are not needed for our use case, but can be added later if needed
 type device struct {
-	ID         int64      `json:"id"` // Traccar device ID
+	LastUpdate *time.Time `json:"lastUpdate,omitempty"`
+	PositionID *int64     `json:"positionId,omitempty"`
 	Name       string     `json:"name"`
 	UniqueID   string     `json:"uniqueId"` // HW or protocol-based unique identifier
 	Status     string     `json:"status"`   // online|offline|unknown
-	LastUpdate *time.Time `json:"lastUpdate,omitempty"`
-	PositionID *int64     `json:"positionId,omitempty"`
+	ID         int64      `json:"id"`       // Traccar device ID
 	// GroupID    *int64         `json:"groupId,omitempty"`
 	// Phone      *string        `json:"phone,omitempty"`
 	// Model      *string        `json:"model,omitempty"`
@@ -26,23 +26,25 @@ type device struct {
 }
 
 type position struct {
-	ID         int64     `json:"id"`
-	DeviceID   int64     `json:"deviceId"`
-	Protocol   string    `json:"protocol"`
+	Attributes map[string]any `json:"attributes,omitempty"`
+
 	DeviceTime time.Time `json:"deviceTime"`
-	// FixTime     time.Time      `json:"fixTime"`
 	ServerTime time.Time `json:"serverTime"`
-	Valid      bool      `json:"valid"`
-	Latitude   float64   `json:"latitude"`
-	Longitude  float64   `json:"longitude"`
-	Altitude   float64   `json:"altitude"` // meters
+
+	Protocol    string  `json:"protocol"`
+	GeofenceIDs []int64 `json:"geofenceIds,omitempty"`
+
+	ID        int64   `json:"id"`
+	DeviceID  int64   `json:"deviceId"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Altitude  float64 `json:"altitude"` // meters
 	// Speed      float64   `json:"speed"`    // knots
 	// Course     float64   `json:"course"`   // degrees
 	// Address     string         `json:"address"`
 	Accuracy float64 `json:"accuracy"` // meters
 	// Network     map[string]any `json:"network,omitempty"`
-	GeofenceIDs []int64 `json:"geofenceIds,omitempty"`
-	// Attributes  map[string]any `json:"attributes,omitempty"`
+	Valid bool `json:"valid"`
 }
 
 type event struct {
